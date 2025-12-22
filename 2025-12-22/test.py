ef34ba2d5d5bdec605fd490e5bd8b12160e3fc5b
Navyartist <navyartist7@gmail.com>
2025-12-22 12:59:12 +0900

    note update + class prac

--- 2025-12-22/test.py
+++ 2025-12-22/test.py
@@ -1,4 +1,18 @@
-나 = { 
-    'name' : '나',
-    'age' : 124
+member = { 
+    {
+        'name' : '고양이',
+        'age' : 124,
+        'home' : '대전'
+    },
+    {
+        'name' : '개',
+        'age' : 156,
+        'home' : '대전'
+    },
 }
+
+class member():
+    def __init__(self): # 생성된 객체 자기 자신을 가리키는 문자 self, 없으면 그냥 지역변수가 되어서 클래스에선 필수
+        self.__member_data = {
+            
+        }
