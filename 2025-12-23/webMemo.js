0de87da6d8620735001495836edc8ee391943fc1
Navyartist <navyartist7@gmail.com>
2025-12-23 17:25:23 +0900

    웹에 텍스트 박스 띄우기

--- 2025-12-23/webMemo.js
+++ 2025-12-23/webMemo.js
@@ -1,4 +1,2 @@
-// web Memo.js
-
-const boxHTML = '<div class="my-box">안녕, 나는 박스야. 이건 git push 연습이야</div>'
-document.body.insertAdjacentHTML('beforeend', boxHTML)
+const boxHTML = '<div class="my-box">안녕! 나는 박스야</div>';
+document.body.insertAdjacentHTML('beforeend', boxHTML);
