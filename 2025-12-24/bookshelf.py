73d0edc1eb761b47c71a136ee55e7f7627e0db62
Navyartist <navyartist7@gmail.com>
2025-12-24 17:06:39 +0900

    각 함수의 기능을 설명하는 주석 추가

--- 2025-12-24/bookshelf.py
+++ 2025-12-24/bookshelf.py
@@ -6,7 +6,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))\
 
 file_path = os.path.join(current_dir, 'data.json')
 
-
+# CREATE : 책꽂이에 새로 산 책을 꽂는다
 def 책꽂기():
     도서명 = input("책이름을 입력하세요: ")
     작가 = input("작가를 입력하세요: ")
@@ -34,6 +34,7 @@ def 책꽂기():
         json.dump(책_리스트, f, indent=4, ensure_ascii=False)
         print(f"도서명 {도서명}: 정보를 JSON 파일에 저장 완료!")
 
+# READING : 책의 데이터를 모두 불러와 읽고 원하는 책을 검색한다.
 def 책검색():
     필드 = input("뭘로 검색하실래요? [도서명] [작가] [출판사] [태그]\n")
     # 할일: 사용자가 없는 필드를 쓰면 오류났다며 함수 종료시키는 루틴 추가
@@ -55,6 +56,7 @@ def 책검색():
         else:
             print("찾으시는 책이 없습니다.")
 
+# UPDATE : 수정을 원하는 책 정보가 있으면 찾아서 정보를 수정한다.
 def 책정보_바꾸기():
     파일경로 = file_path
     with open(파일경로, 'r', encoding='utf-8') as f:
@@ -94,5 +96,4 @@ elif 기능선택 == '3':
 #   책이름이 너무 길거나 복잡하면 쓰기가 힘든데, 수정할 책을 선택할 때 ISBN같은 고유코드를 사용해야 할까?
 # 이 코드에 class 쓰려면?
 # 태그를 여러개 쓰고 싶음
-# def 책정리
 # def 책치우기
