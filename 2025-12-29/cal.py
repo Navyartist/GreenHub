a766db98ff939b88e18c698864a782b8928c466c
Navyartist <navyartist7@gmail.com>
2025-12-29 16:20:33 +0900

    버튼 누르기 동작-지정한 커맨드의 함수 실행

--- 2025-12-29/cal.py
+++ 2025-12-29/cal.py
@@ -4,26 +4,19 @@ from tkinter import ttk
 
 키패드 = Tk()
 frm = ttk.Frame(키패드, padding=10)
-
 frm.grid()
+msg = StringVar(value="hi")
 
-기호 = '+'
-숫자1 = '1'
-숫자2 = '3'
+def change_msg():
+    msg.set("Hello")
 
-ttk.Label(frm, text="calculator").grid(column=0, row=5)
-ttk.Button(frm, text="Quit", command=키패드.destroy).grid(column=1, row=5)
+ttk.Label(frm, textvariable=msg).grid(column=0, row=0)
+ttk.Label(frm, text="Calculator").grid(column=0, row=3)
+ttk.Button(frm, text="Quit", command=키패드.destroy).grid(column=1, row=3)
+ttk.Button(frm, text=f"1", command=change_msg).grid(row=1,column=0)
+ttk.Button(frm, text=f"2", command=change_msg).grid(row=1,column=1)
+ttk.Button(frm, text=f"3", command=change_msg).grid(row=1,column=2)
+ttk.Button(frm, text=f"4", command=change_msg).grid(row=1,column=3)
 
-ttk.Button(frm, text=f"1").grid(row=0,column=0)
-ttk.Button(frm, text=f"2").grid(row=0,column=1)
-ttk.Button(frm, text=f"3").grid(row=0,column=2)
-
-ttk.Button(frm, text=f"4").grid(row=1,column=0)
-ttk.Button(frm, text=f"5").grid(row=1,column=1)
-ttk.Button(frm, text=f"6").grid(row=1,column=2)
-
-ttk.Button(frm, text=f"7").grid(row=2,column=0)
-ttk.Button(frm, text=f"8").grid(row=2,column=1)
-ttk.Button(frm, text=f"9").grid(row=2,column=2)
 
 키패드.mainloop()
