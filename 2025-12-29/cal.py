2ae1998a72d599d04031b45a7c850fb54de1f69f
Navyartist <navyartist7@gmail.com>
2025-12-29 16:54:04 +0900

    더이상 cal이 아니다

--- 2025-12-29/cal.py
+++ 2025-12-29/cal.py
@@ -7,16 +7,25 @@ frm = ttk.Frame(키패드, padding=10)
 frm.grid()
 msg = StringVar(value="hi")
 
-def change_msg():
+def change_msg1():
     msg.set("Hello")
+
+def change_msg2():
+    msg.set("World")
+
+def change_msg3():
+    msg.set("Happy")
+
+def change_msg4():
+    msg.set("Comma")
 
 ttk.Label(frm, textvariable=msg).grid(column=0, row=0)
 ttk.Label(frm, text="Calculator").grid(column=0, row=3)
 ttk.Button(frm, text="Quit", command=키패드.destroy).grid(column=1, row=3)
-ttk.Button(frm, text=f"1", command=change_msg).grid(row=1,column=0)
-ttk.Button(frm, text=f"2", command=change_msg).grid(row=1,column=1)
-ttk.Button(frm, text=f"3", command=change_msg).grid(row=1,column=2)
-ttk.Button(frm, text=f"4", command=change_msg).grid(row=1,column=3)
+ttk.Button(frm, text=f"1", command=change_msg1).grid(row=1,column=0)
+ttk.Button(frm, text=f"2", command=change_msg2).grid(row=1,column=1)
+ttk.Button(frm, text=f"3", command=change_msg3).grid(row=1,column=2)
+ttk.Button(frm, text=f"4", command=change_msg4).grid(row=1,column=3)
 
 
 키패드.mainloop()
